1ce567370d0c5bb15f8181b736c7dfb08ebed577
gokul <gokul@estate.one>
2020-01-23 22:16:02 +0530

    FIX

--- script.sh
+++ script.sh
@@ -1,36 +1,36 @@
 # creating folders
 
-#default home directory 
-home=/home/ec2-user
+#default home directory
+cwd=$(pwd)
 #Creating Default Folder
-mkdir $home/default
-sudo touch $home/default/a.txt
-sudo touch $home/default/c.txt
-sudo touch $home/default/e.txt
+mkdir $cwd/default
+touch $cwd/default/a.txt
+touch $cwd/default/c.txt
+touch $cwd/default/e.txt
 
 # Creating folder 1
-mkdir $home/1
-sudo touch $home/1/b.txt
-sudo touch $home/1/c.txt
-sudo touch $home/1/f.txt
+mkdir $cwd/1
+touch $cwd/1/b.txt
+touch $cwd/1/c.txt
+touch $cwd/1/f.txt
 
 #Creating folder 2
-mkdir $home/2
-sudo touch $home/2/a.txt
-sudo touch $home/2/c.txt
-sudo touch $home/2/d.txt
+mkdir $cwd/2
+touch $cwd/2/a.txt
+touch $cwd/2/c.txt
+touch $cwd/2/d.txt
 
 #Creating folder 3
-mkdir $home/3
-sudo touch $home/3/g.txt
-sudo touch $home/3/a.txt
-sudo touch $home/3/d.txt
+mkdir $cwd/3
+touch $cwd/3/g.txt
+touch $cwd/3/a.txt
+touch $cwd/3/d.txt
 
 #Creating folder 4
-mkdir $home/4
-sudo touch $home/4/h.txt
-sudo touch $home/4/a.txt
-sudo touch $home/4/b.txt
+mkdir $cwd/4
+touch $cwd/4/h.txt
+touch $cwd/4/a.txt
+touch $cwd/4/b.txt
 
 #declaring array of files
 a=( a.txt b.txt c.txt d.txt e.txt f.txt g.txt h.txt )
@@ -41,29 +41,26 @@ f=( default 4 2 1 3 )
 # Display the files
 echo "These are the files present"
 for i in "${a[@]}"
- do 
+ do
   echo "$i "
  done
 
 #Remove the older rexult file and creating a new one
 rm -rf result.txt
-
-sudo touch result.txt
-
-
+touch result.txt
 
 
 #check files name wise
 for i in "${a[@]}"
- do 
+ do
  for j in "${f[@]}"
-   do
-   
-    FILE=$home/${f[$j]}/$i
+ do
+
+    FILE=$cwd/${f[$j]}/$i
     if test -f "$FILE"; then
-       echo "$i	::	$FILE" >> $home/result.txt
+       echo "$i	::  $FILE" >> $cwd/result.txt
        break
     fi
    done
-done	
-echo "results stored in result.txt in file :: filepath format" 
+done
+echo "results stored in result.txt in file :: filepath format"
